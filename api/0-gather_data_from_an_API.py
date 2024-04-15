c779711ef6eaf05d3d5780a1f3b4d9a2b9f128e6
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-15 12:18:43 -0400

    task 1 & 2

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import sys
+
+employee_id = sys.argv[1]
+
+employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
+employee_data_json = employee_data.json()
+
+employee_name = employee_data_json['name']
+
+todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+todo_data_json = todo_data.json()
+
+total_todos = str(len(todo_data_json))
+completed_todos = str(sum(1 for task in todo_data_json if task ['completed']))
+
+print("Employee " + employee_name + " is done with tasks(" +
+      completed_todos + "/" + total_todos + "):")
+
+for task in todo_data_json:
+      if task['completed']:
+            print('\t ' + task['title'])
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import csv
+import requests
+import sys
+
+employee_id = sys.argv[1]
+
+employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
+employee_data_json = employee_data.json()
+
+employee_name = employee_data_json['name']
+
+todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+todo_data_json = todo_data.json()
+
+with open (f'{employee_id}.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    for task in todo_data_json:
+        writer.writerow([employee_id, employee_name, task['completed'], task['title']])
