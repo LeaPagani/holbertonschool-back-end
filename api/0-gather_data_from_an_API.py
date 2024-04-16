0fd6aff9b800713a852f849766bb329205887865
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-16 09:20:51 -0400

    documenting tasks files

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
+"""
+For a given employee ID, returns information about his/her task list progress
+"""
 
 import json
 import requests
 import sys
 
+# Get the employee id from command line
 employee_id = sys.argv[1]
 
+# Get request to api for employee data
 employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
+
+# Parse data as json
 employee_data_json = employee_data.json()
 
+# Get employee name from key name
 employee_name = employee_data_json['name']
 
+# Get request to api for todo data
 todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+
+# Parse data as json
 todo_data_json = todo_data.json()
 
+# Use len to calculate total number of tasks
 total_todos = str(len(todo_data_json))
+
+# Calculate completed tasks
 completed_todos = str(sum(1 for task in todo_data_json if task ['completed']))
 
+# Print output with provided format
 print("Employee " + employee_name + " is done with tasks(" +
       completed_todos + "/" + total_todos + "):")
 
+# List completed tasks titles
 for task in todo_data_json:
       if task['completed']:
             print('\t ' + task['title'])
 
+# Check if the script is being run directly as the main program
 if __name__ == '__main__':
-    pass
+      # Code inside this block will only run if this script is executed directly
+      pass
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python3
+"""
+Python script to export data in the JSON format
+"""
 
 import json
 import requests
 
+# Get request to api for employee data
 employee_data = requests.get('https://jsonplaceholder.typicode.com/users')
+
+# Parse data as json
 employee_data_json = employee_data.json()
 
+# Create empty all_tasks dictionary
 all_tasks = {}
 
+# Iterate over each user in the employee_data_json list
 for user in employee_data_json:
+    # Extract the user ID and username from the current user
     user_id = str(user['id'])
     user_name = user['username']
+    # Get request to api for todo data of current user
     todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + user_id)
+    # Parse data as json
     todo_data_json = todo_data.json()
+    # Task list with provided format
     task_list = [
         {
             'username': user_name,
             'task': task['title'],
             'completed': task['completed']
         } for task in todo_data_json]
+    # Adds the task to the all_tasks dictionary
     all_tasks[user_id] = task_list
 
+# Export the all_tasks dictionary to a JSON file named 'todo_all_employees.json'
 with open('todo_all_employees.json', 'w') as jsonfile:
     json.dump(all_tasks, jsonfile)
 
+# Check if the script is being run directly as the main program
 if __name__ == '__main__':
-    pass
+      # Code inside this block will only run if this script is executed directly
+      pass
