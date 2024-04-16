44ed36c63d36cffccdd75fe61618a8b7be8bb599
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-16 19:49:50 -0400

    changing format for task 2 & 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,45 +1,40 @@
 #!/usr/bin/python3
 """
-Python script to export data in the JSON format
+This module uses Python to make requests to a REST API.
+It fetches data about a specific employee's tasks
+and prints a summary of the tasks completed and the
+titles of the completed tasks.
 """
-
 import json
 import requests
 
-# Get request to api for employee data
-employee_data = requests.get('https://jsonplaceholder.typicode.com/users')
 
-# Parse data as json
-employee_data_json = employee_data.json()
+# Reuet data for users
+users = requests.get('https://jsonplaceholder.typicode.com/users').json()
 
-# Create empty all_tasks dictionary
+# Make an empty dictionary
 all_tasks = {}
 
-# Iterate over each user in the employee_data_json list
-for user in employee_data_json:
-    # Extract the user ID and username from the current user
-    user_id = str(user['id'])
+# Loop over all users
+for user in users:
+    # get the user id and name
+    user_id = user['id']
     user_name = user['username']
-    # Get request to api for todo data of current user
-    todo_data = requests.get(
-         'https://jsonplaceholder.typicode.com/todos?userId=' + user_id)
-    # Parse data as json
-    todo_data_json = todo_data.json()
-    # Task list with provided format
-    task_list = [
-        {
-            'username': user_name,
-            'task': task['title'],
-            'completed': task['completed']
-        } for task in todo_data_json]
-    # Adds the task to the all_tasks dictionary
+
+    # Get the list
+    todo_list = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={user_id}').json()
+
+    # Format the task as indicated
+    task_list = [{'username': user_name, 'task': task.get(
+        'title'), 'completed': task.get('completed')} for task in todo_list]
+
+    # Adds the task to the dictionary
     all_tasks[user_id] = task_list
 
-# Export all_tasks dictionary to a jsonfile named 'todo_all_employees.json'
+# Export using Json format
 with open('todo_all_employees.json', 'w') as jsonfile:
     json.dump(all_tasks, jsonfile)
 
-# Check if the script is being run directly as the main program
 if __name__ == '__main__':
-    # Code inside this block will only run if this script is executed directly
     pass
